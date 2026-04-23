cceb4f04633e6bda075630f2fd4294c13cf91291
faradila <151923904+faradila16@users.noreply.github.com>
2026-04-23 12:08:51 +0700

    tgas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lyonaa24 | Bondowoso jawa timur Florist</title>
+    <title>Lyonaa24 | jurang sapi Bondowoso jawa timur Florist</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
     
